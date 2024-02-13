0b78472cb1f00966f4b7557d2b491d052a5540ca
Lefuden <lefuden@gmail.com>
2024-02-13 10:31:25 +0100

    init

---
+++ src/components/BookDisplay.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import bookData from '../enkeldata.json';
+
+const BookDisplay = () => {
+  return (
+    <>
+    <div>
+      <h1>Mina böcker</h1>
+      {
+        bookData.böcker.map((bok) => (
+          <div key={bok.id}>
+            <h2>{bok.titel}</h2>
+            <p>Författare: {bok.författare}</p>
+            <p>Utgivningsår: {bok.utgivningsår}</p>
+            <p>Genre: {bok.genre}</p>
+          </div>
+        ))}
+    </div>
+    <div>
+    <h1>Mina cdskivor</h1>
+    {
+      bookData.cdskivor.map((cd) => (
+        <div key={cd.id}>
+          <h2>{cd.album}</h2>
+          <p>Artist: {cd.artist}</p>
+          <p>Utgivningsår: {cd.utgivningsår}</p>
+          <p>Genre: {cd.genre}</p>
+        </div>
+      ))}
+  </div>
+    </>
+
+  );
+};
+export default BookDisplay;
